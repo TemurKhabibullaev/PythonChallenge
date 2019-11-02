2c4b534244579ff377c93b1b5a1ec5dc1ebb6eb2
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2019-11-02 14:02:05 -0400

    Exercises

--- multiple_exercises.py
+++ multiple_exercises.py
@@ -53,3 +53,26 @@ if persons_birthday == "Temur":
     print(birthdays["Temur"])
 if persons_birthday == "Jared":
     print(birthdays["Jared"])
+else:
+    print("I don't have his birthday.")
+
+
+# 5) Take a list, say for example this one:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+    if i < 5:
+        print(i)
+    else:
+        break
+
+
+# 6) Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+# (If you don’t know what a divisor is, it is a number that divides evenly into another number.
+# For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+your_number = int(input("Give me a number, and I'll tell you its divisors!"))
+eve = list(range(1, your_number + 1))
+div_num = []
+w = your_number % eve
+
